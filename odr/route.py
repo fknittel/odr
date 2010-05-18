2cd4b68063a0793b81fd0e0619cfb62e5ba80186
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2010-05-18 22:46:01 +0200

    odr/route.py: add file missing from last commit

---
+++ odr/route.py
@@ -0,0 +1,31 @@
+# vim:set encoding=utf-8 ft=python ts=8 sw=4 sts=4 et cindent:
+
+# route.py -- Provides network mask calculations.
+#
+# Copyright © 2010 Fabian Knittel <fabian.knittel@avona.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def network_mask(mask_width):
+    """Build the network mask matching the specified network mask bit width.
+
+    @return: Returns the network mask as list of integers.
+    """
+    mask = [255] * max(mask_width / 8, 0)
+    if len(mask) < 4:
+        mask += [255 - (2**(8 - (mask_width % 8)) - 1)]
+    mask += [0] * (4 - len(mask))
+    return mask
+
